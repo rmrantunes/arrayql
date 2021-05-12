3d12fc2a84f1dd97e8d4fc2decc3c81b5df76a20
Rafael Antunes <rafaelmessias710@gmail.com>
2021-05-12 17:57:48 -0400

    feat: initial working structure finished

--- src/arrayql.ts
+++ src/arrayql.ts
@@ -1,16 +1,39 @@
-import { QueryOptions } from ".";
+import { BooleanKeys, QueryOptions } from ".";
 
-function objectQL<T>(keys: string[], object: T) {
-  return keys.reduce((returned, key) => {
-    return { ...returned, [key]: object[key] };
+function isObject(item: any) {
+  return Object.prototype.toString.call(item).includes("Object");
+}
+
+function isArray(item: any) {
+  return Array.isArray(item);
+}
+
+function objectQL<T>(object: T, optionsKeysObject: BooleanKeys<T>) {
+  const queriedObjectKeys = Object.keys(optionsKeysObject);
+
+  return queriedObjectKeys.reduce((returned, key) => {
+    const prop = object[key];
+    const isPropArray = isArray(prop);
+    const isPropObject = isObject(prop);
+    const recursiveOptionsKeysObject = { ...optionsKeysObject }[key];
+
+    const value = isPropObject
+      ? objectQL(prop, recursiveOptionsKeysObject)
+      : isPropArray
+      ? arrayQL(prop, recursiveOptionsKeysObject)
+      : prop;
+
+    return { ...returned, [key]: value };
   }, {});
 }
 
-function arrayQL<T, R>(array: T[], queryOptions: QueryOptions<T>): R | any {
+export function arrayQL<T, R = any>(
+  array: T[],
+  queryOptions: QueryOptions<T>
+): R[] {
   const returned = array.map((object) => {
-    const keys = Object.keys(queryOptions.keys);
-    // if array recursive, if object
-    return queryOptions.where(object) && objectQL(keys, object);
+    queryOptions.where ??= () => true;
+    return queryOptions.where(object) && objectQL(object, queryOptions.keys);
   });
 
   return returned.filter(Boolean);
