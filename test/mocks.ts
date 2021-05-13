88206f474fdbbed68855c33fa44be0ddd8cbc1f7
Rafael Antunes <rafaelmessias710@gmail.com>
2021-05-13 07:46:30 -0400

    chore: add some test mocks

---
+++ test/mocks.ts
@@ -0,0 +1,44 @@
+export const unorganizedData = [
+  {
+    name: "carl",
+    age: 18,
+    address: {
+      street: "of nations",
+      city: "jipa",
+      adcionalInfo: {
+        title: "i dont know",
+        subtitle: "subtitle",
+      },
+    },
+    tags: ["array", "filter"],
+    friends: [
+      {
+        name: "jamelin",
+        id: 1,
+      },
+    ],
+  },
+  {
+    name: "juca",
+    age: 30,
+    address: {
+      street: "das dorgas",
+      city: "github",
+      adcionalInfo: {
+        title: "sei lá",
+        subtitle: "subtitle",
+      },
+    },
+    tags: ["array", "filter"],
+    friends: [
+      {
+        name: "jamelão",
+        id: 1,
+      },
+      {
+        name: "rafael",
+        id: 9,
+      },
+    ],
+  },
+];
