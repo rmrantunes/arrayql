75d2d5231b069a2bc3eb92c04d9a7303d54af1e0
Rafael Antunes <rafaelmessias710@gmail.com>
2021-05-15 20:10:14 -0400

    type: add declare module

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,30 +1,29 @@
-// export type AllKeys = { __allKeys: boolean };
-// BooleanKeys<T[P]> | AllKeys
+declare module "arrayql-js" {
+  type PrimitiveOrFunction =
+    | string
+    | number
+    | boolean
+    | null
+    | undefined
+    | (() => any);
 
-type PrimitiveOrFunction =
-  | string
-  | number
-  | boolean
-  | null
-  | undefined
-  | (() => any);
+  export type BooleanKeys<T> = {
+    [P in keyof T]?: T[P] extends any[]
+      ? T[P] extends PrimitiveOrFunction[]
+        ? boolean
+        : QueryOptions<T[P][number]>
+      : T[P] extends Record<string, any>
+      ? BooleanKeys<T[P]>
+      : boolean;
+  };
 
-export type BooleanKeys<T> = {
-  [P in keyof T]?: T[P] extends any[]
-    ? T[P] extends PrimitiveOrFunction[]
-      ? boolean
-      : QueryOptions<T[P][number]>
-    : T[P] extends Record<string, any>
-    ? BooleanKeys<T[P]>
-    : boolean;
-};
+  export interface QueryOptions<T> {
+    where?: (object: T) => boolean;
+    keys: BooleanKeys<T>;
+  }
 
-export interface QueryOptions<T> {
-  where?: (object: T) => boolean;
-  keys: BooleanKeys<T>;
+  export function arrayQL<T, R = any>(
+    array: T[],
+    queryOptions: QueryOptions<T>
+  ): R[];
 }
-
-export function arrayQL<T, R = any>(
-  array: T[],
-  queryOptions: QueryOptions<T>
-): R[];
